9cfdebd6172199e265fb7e0436c5d58d901f5c36
Nathan Birindwa <birhindwan@gmail.com>
2025-07-20 19:48:33 +0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import '.././global.css';
+
+export default function Index() {
+  return (
+    <SafeAreaView>
+      <View className="">
+        <Text className="">Hello</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
